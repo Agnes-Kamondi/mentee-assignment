4b2147ee209f31e093af2501c1ac3448a78c7d3a
Agnes-Kamondi <kamondiagnesauma@gmail.com>
2024-03-20 20:45:35 +0300

    modified my code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,33 +1,31 @@
 fun main() {
-   var school = checkLength("Akirachix")
+   println( checkLength("Akirachix"))
 
-    var name1 =name("Agnes")
-       println(name1.uppercase())
+   println(name("Agnes"))
 
-    var place =give("Naivasha")
+    println(give("Naivasha"))
 
-   var present= here("Nairobi")
+   println(here("Nairobi"))
 
-    merge("Agnes","Nairobi")
+   println(merge("Agnes","Nairobi"))
 
-    equality("Write","write")
+    println(equality("Write","write"))
 
-    compre("people","person")
+  println(compre("people","person"))
 
-    weekday(4)
+    println(weekday(4))
 
-    findSubstringIndex("here","there")
+    println(findSubstringIndex("here","there"))
 
-    removeVowels("banana")
+    println(removeVowels("banana"))
 
-    removeConsonants("calibraces")
+    println(removeConsonants("calibraces"))
 
 }
 //write a kotlin function that takes in a string and returns the length.
 fun checkLength(school:String): Int{
-   var  size = school.length
-    println(size)
-    return size
+   return school.length
+//    return school.length
 }
 // write a kotlin function that takes a string as an input and returns the string in uppercase letters.
 fun name(name1:String): String{
@@ -35,39 +33,26 @@ fun name(name1:String): String{
 }
 //write a kotlin function that takes a string as an input and returns the string in lowercase letters.
 fun give (place:String): String{
-   var county =place.lowercase()
-    println(county)
-    return county
+    return place.lowercase()
 }
 //Write a function that takes a string as input and returns the last character of the string.
 fun here (present:String): Char{
-    var presently = present.last()
-    return presently
+    return present.last()
 }
 //Write a function that takes two strings as input and concatenates them together.
 fun merge (three:String,four:String): String{
     var result = three.plus(four)
-    println(result)
     return result
 }
 //Write a function that takes two strings as input and checks if they are equal.
 //method1
 fun equality (five: String,six:String):Boolean{
-    if (five == six)
-    {
-        println(true)
-    }
-    else{
-        println(false)
-    }
     var seven = five == six
-    println(seven)
     return seven
 }
 //method2
-fun compre (most:String, less:String):Int {
-     var result1 = most.compareTo(less)
-     return result1
+fun compre (most:String, less:String):Boolean {
+     return most.equals(less)
 }
 //use the weekday number to calculate the weekday name :
 fun weekday (weekDay : Int): String {
@@ -81,7 +66,6 @@ fun weekday (weekDay : Int): String {
         7 -> "Saturday"
         else -> "Invalid name"
     }
-    println(result3)
     return result3
 
 }
@@ -90,19 +74,16 @@ fun weekday (weekDay : Int): String {
 // the function should return -1.
 fun findSubstringIndex(string1:String,string2:String):Int{
     var result4 = string1.indexOf(string2)
-    println(result4)
    return result4
 }
 //Write a function that takes a string as input and returns a new string with all the vowels removed.
 fun removeVowels(input:String):String{
  var result5 = input.filter{ it !in "aeiouAEIOU"}
-    println(result5)
     return result5
 }
 //Write a function that takes a string as input and returns a new string with all the consonants removed.
 fun removeConsonants(bracket:String):String{
     var result6 = bracket.filter{it !in "bcdfghjklmnpqrstvwxyz"}
-    println(result6)
     return(result6)
 }
 
